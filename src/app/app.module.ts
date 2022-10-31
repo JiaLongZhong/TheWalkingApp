f9b15fe1ee8687d1b8577dd3a545e39fbf62e3f0
Marcos Freire <mfreire1996@gmail.com>
2022-10-31 18:09:09 -0400

    Front End Side of Walking App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShopComponent } from './shop/shop.component';
+import { OwnerComponent } from './owner/owner.component';
+import { ManagerComponent } from './manager/manager.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    OwnerComponent,
+    ManagerComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
